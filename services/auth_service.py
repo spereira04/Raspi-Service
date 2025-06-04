b268df6a1a52ade2e3dce3220a5523e92febbfef
spereira04 <santipereira1987@gmail.com>
2025-06-04 16:39:26 -0300

    small fixes

--- services/auth_service.py
+++ services/auth_service.py
@@ -3,24 +3,22 @@ import json
 from config.raspi import Raspi
 
 class AuthService:
-
-	baseUrl: str
-	raspi : Raspi
+    baseUrl: str
+    raspi: Raspi
+	
 
     def __init__(self, baseUrl, raspi):
         self.baseUrl = baseUrl
         self.raspi = raspi
 
     def log_in(self):
-    	try:
-    		data = {'doorName': raspi.door_name, 'passcode': raspi.passcode};
-
+        try:
+            data = {'doorName': self.raspi.door_name, 'passcode': self.raspi.passcode}
             response = requests.post(url=self.baseUrl+'/doors/connect', data=data)
-            raspi.set_access_token(response.json()['token'])
-
+            self.raspi.set_access_token(response.json()['token'])
             return True
-           
         except requests.exceptions.ConnectionError:
             print("No connection to the User Service")
             return False
+    
         
