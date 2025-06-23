4622e765ce10c82dff536b3d65a1135fee98ee3f
ftambler <federicotambler@gmail.com>
2025-06-22 21:38:17 -0300

    tz fix

--- services/user_vectors_service.py
+++ services/user_vectors_service.py
@@ -26,7 +26,7 @@ class UserVectorsService:
         self.raspi = raspi
 
     def look_for_similar_user_vector(self, buffer):
-        access_time = int(time.time() + 3*60*60*1000)
+        access_time = int(time.time())
 
         embedding = self.ibed.to_embeddings(Image.open(io.BytesIO(buffer)))[0]
         
